6df3ab8ab5c9c3c348eb178f1519b51c8d8f266a
nkohlmeier <nkohlmeier@users.noreply.github.com>
2017-12-21 15:32:01 +0100

    NotificationListener angefangen

--- SmartJacket/app/src/main/java/fh/com/smartjacket/fragment/SettingsFragment.java
+++ SmartJacket/app/src/main/java/fh/com/smartjacket/fragment/SettingsFragment.java
@@ -5,22 +5,36 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import fh.com.smartjacket.R;
+import fh.com.smartjacket.adapter.AppNotificationListAdapter;
+import fh.com.smartjacket.pojo.AppVibrationConfig;
 
 /**
  *
  */
 public class SettingsFragment extends Fragment {
+	private AppNotificationListAdapter adapter;
+	private ArrayList<AppVibrationConfig> apps = new ArrayList<>();
 
 	public SettingsFragment() {
 		// Required empty public constructor
+		this.apps.add(new AppVibrationConfig("Peter"));
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_settings, container, false);
+		View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+		ListView appListView = view.findViewById(R.id.fragmentSettingsAppNotificationListView);
+		this.adapter = new AppNotificationListAdapter(getActivity(), this.apps);
+		appListView.setAdapter(this.adapter);
+
+		return view;
 	}
 }
